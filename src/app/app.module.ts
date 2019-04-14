1d1fe66437ce7de4f645829cfcd3ad32dd9e322e
trGalho <trgalho@debianDell.WORKGROUP>
2019-04-14 16:50:03 -0300

    Algumas modificações no example do curso da Udemy e teste de outros compontes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { FeedPageModule } from '../pages/feed/feed.module';
+import { IntroPageModule } from '../pages/intro/intro.module';
 
 @NgModule({
   declarations: [
@@ -17,10 +19,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage    
   ],
   imports: [
     BrowserModule,
+    FeedPageModule,
+    IntroPageModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { IntroPage } from '../intro/intro';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  intro = IntroPage;
   constructor(public navCtrl: NavController) {
 
   }
